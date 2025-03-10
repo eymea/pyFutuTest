cf774168bcb1572e2f7b8bd74e98435617daf8ba
eymea <eduardo@eymea.com>
2025-03-10 22:36:12 +1100

    v3

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -47,7 +47,7 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Futu Test! v2')
+        'body': json.dumps('Hello from Futu Test! v3')
     }
 
     if(success):
