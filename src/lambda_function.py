8af01d3bf78f231a1c1995321df15a02fd6e8c16
eymea <eduardo@eymea.com>
2025-03-10 23:02:12 +1100

    Second Commit

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -47,7 +47,7 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Futu Test! v5')
+        'body': json.dumps('Hello from Futu Test! v2')
     }
 
     if(success):
